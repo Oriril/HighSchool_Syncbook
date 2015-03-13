58afa396af5df987017736ff1e556228617c8dac
Enrico <enrico.basso01@gmail.com>
2015-03-13 16:58:23 +0100

    Fixed the navabar and modified feedback.

--- src/huge/application/view/_templates/feedback.php
+++ src/huge/application/view/_templates/feedback.php
@@ -7,13 +7,21 @@ $feedback_negative = Session::get('feedback_negative');
 // echo out positive messages
 if (isset($feedback_positive)) {
     foreach ($feedback_positive as $feedback) {
-        echo '<div class="feedback success">'.$feedback.'</div>';
+        echo '<div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>' . $feedback.'
+                </div>';
     }
 }
 
 // echo out negative messages
 if (isset($feedback_negative)) {
     foreach ($feedback_negative as $feedback) {
-        echo '<div class="feedback error">'.$feedback.'</div>';
+        echo '<div class="alert alert-danger alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>' . $feedback.'
+                </div>';
     }
 }
--- src/huge/application/view/_templates/header.php
+++ src/huge/application/view/_templates/header.php
@@ -21,10 +21,10 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationInclude.php
     <![endif]-->
 </head>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-top-fixed">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -87,3 +87,4 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationInclude.php
     </div>
 </nav>
 <div class="container">
+    <?php $this->renderFeedbackMessages(); ?>
