4b67e3093230986fb3f19d2e1d8c036219684a5d
Enrico <enrico.basso01@gmail.com>
2015-03-18 17:06:48 +0100

    Login layout modified.

--- src/huge/application/view/login/index.php
+++ src/huge/application/view/login/index.php
@@ -3,30 +3,53 @@
     <!-- echo out the system feedback (error and success messages) -->
     <?php //$this->renderFeedbackMessages(); ?>
 
-    <div class="login-page-box">
-        <div class="table-wrapper">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="well">
+                <!-- login box on left side -->
+                <form class="form-horizontal" action="<?php echo Config::get('URL'); ?>login/login" method="post">
+                    <fieldset>
+                        <legend>Login here</legend>
 
-            <!-- login box on left side -->
-            <div class="login-box">
-                <h2>Login here</h2>
-                <form action="<?php echo Config::get('URL'); ?>login/login" method="post">
-                    <input type="text" name="user_name" placeholder="Username or email" required />
-                    <input type="password" name="user_password" placeholder="Password" required />
-                    <label for="set_remember_me_cookie" class="remember-me-label">
-                        <input type="checkbox" name="set_remember_me_cookie" class="remember-me-checkbox" />
-                        Remember me for 2 weeks
-                    </label>
-                    <input type="submit" class="login-submit-button" value="Log in"/>
+                        <div class="form-group">
+                            <label for="user_name" class="col-lg-2 control-label">Username or email</label>
+                            <div class="col-lg-10">
+                                <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Username or email" required />
+                            </div>
+                            <label for="user_password" class="col-lg-2 control-label">Password</label>
+                            <div class="col-lg-10">
+                                <input type="password" class="form-control" id="user_password" name="user_password" placeholder="Password" required />
+                            </div>
+
+                        </div>
+                        <div class="form-group">
+                            <label for="set_remember_me_cookie" class="col-lg-2 control-label">
+                                <input type="checkbox" name="set_remember_me_cookie" class="remember-me-checkbox" />
+                                Remember me for 2 weeks
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-lg-10">
+                                <input type="submit" class="btn-primary" value="Log in"/>
+                            </div>
+                        </div>
+                    </fieldset>
                 </form>
                 <div class="link-forgot-my-password">
                     <a href="<?php echo Config::get('URL'); ?>login/requestPasswordReset">I forgot my password</a>
                 </div>
             </div>
-
-            <!-- register box on right side -->
-            <div class="register-box">
-                <h2>No account yet ?</h2>
-                <a href="<?php echo Config::get('URL'); ?>login/register">Register</a>
+        </div>
+        <div class="col-sm-6">
+            <div class="well">
+                <form class="form-horizontal">
+                    <fieldset>
+                        <legend>No account yet ?</legend>
+                        <div class="col-lg-10">
+                            <a href="<?php echo Config::get('URL'); ?>login/register">Register!</a>
+                        </div>
+                    </fieldset>
+                </form>
             </div>
 
         </div>
