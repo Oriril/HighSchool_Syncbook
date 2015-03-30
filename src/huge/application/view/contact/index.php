8413a898e5092a15f35350f5b1be2bb3d0b85414
Enrico <enrico.basso01@gmail.com>
2015-03-30 20:46:23 +0200

    Trying some stuff [Pt. 2].

---
+++ src/huge/application/view/contact/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php //$this->renderFeedbackMessages(); ?>
+    <div class="row">
+        <p>Hola</p>
+    </div>
+</div>
