175da5fc64280696d9e8b219425fcabcbf46d840
Enrico <enrico.basso01@gmail.com>
2015-04-02 11:57:53 +0200

    Ordinary stuff (?)

--- src/huge/application/view/dashboard/index.php
+++ src/huge/application/view/dashboard/index.php
@@ -1,15 +1,29 @@
 <div class="container">
-    <h1>DashboardController/index</h1>
-    <div class="box">
-
-        <!-- echo out the system feedback (error and success messages) -->
-        <?php //$this->renderFeedbackMessages(); ?>
-
-        <h3>What happens here ?</h3>
-        <p>
-            This is an area that's only visible for logged in users. Try to log out, an go to /dashboard/ again. You'll
-            be redirected to /index/ as you are not logged in. You can protect a whole section in your app within the
-            according controller by placing <i>Auth::handleLogin();</i> into the constructor.
-        <p>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Actions</h3>
+                </div>
+                <div class="panel-body">
+                    <ul class="list-group">
+                        <a href="<?php echo Config::get('URL'); ?>contact/addcontact" class="list-group-item">
+                            <h4 class="list-group-item-heading">Add contact</h4>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <h4 class="list-group-item-heading">Show contacts</h4>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <div class="panel panel-info">
+                <div class="panel-heading">News</div>
+                <div class="panel-body">
+                    Panel content
+                </div>
+            </div>
+        </div>
     </div>
 </div>
