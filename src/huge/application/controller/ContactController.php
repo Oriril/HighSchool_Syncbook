7d525f4ac21c63b54d9f57c1071d503c8f20a5b6
Federico Longhin <longhin.federico.nildric@gmail.com>
2015-04-08 09:49:36 +0200

    Added vCard Creation procedure from addContact Form

--- src/huge/application/controller/ContactController.php
+++ src/huge/application/controller/ContactController.php
@@ -1,4 +1,10 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationInclude.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationClass.php");
+
+require(SOURCE_PATH . "SabreDAV/CardDAV/cardDAVManagement.php");
+require(SOURCE_PATH . "SabreDAV/CardDAV/contactMapper.php");
+require(SOURCE_PATH . "SabreDAV/CardDAV/vCardManagement.php");
 
 class ContactController extends Controller {
 
@@ -27,8 +33,17 @@ class ContactController extends Controller {
     }
 
     public function insertNewContact() {
-        if (ContactModel::buildNewContact()) {
+        // Getting PDO Connection for User
+        $connectionPDO = databaseSabreDAVConnectPDO(Session::get('user_name'), new configurationClass());
 
+        // Retrieving AddressBook "Contacts" for User
+        $addressBook = cardDAVAddressBookRetrieve($connectionPDO, Session::get('user_name'), "Contacts");
+        // Checking if all went well
+        if ($addressBook !== FALSE) {
+            // Mapping vCardData from insertContact Form
+            $vCardData = mapperObjectCard(json_decode(ContactModel::buildNewContact()));
+            // Inserting vCard into Database
+            vCardCreate($addressBook, $vCardData);
         }
     }
 } 
