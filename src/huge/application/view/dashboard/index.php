dcf800d28e643f22bc9cbec2f1fd4382e155490c
Enrico <enrico.basso01@gmail.com>
2015-04-09 18:06:23 +0200

    A little bit of JQuery arrived.

--- src/huge/application/view/dashboard/index.php
+++ src/huge/application/view/dashboard/index.php
@@ -1,23 +1,23 @@
 <div class="container">
     <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-2">
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <h3 class="panel-title">Actions</h3>
                 </div>
                 <div class="panel-body">
                     <ul class="list-group">
-                        <a href="<?php echo Config::get('URL'); ?>contact/addcontact" class="list-group-item">
-                            <h4 class="list-group-item-heading">Add contact</h4>
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <h4 class="list-group-item-heading">Show contacts</h4>
-                        </a>
+                        <div class="list-group-item " id="displayAddContactForm">
+                            <div class="list-group-item-heading"><strong>Add contact</strong></div>
+                        </div>
+                        <div class="list-group-item ">
+                            <div class="list-group-item-heading"><strong>Contacts</strong></div>
+                        </div>
                     </ul>
                 </div>
             </div>
         </div>
-        <div class="col-sm-8">
+        <div class="col-sm-10" id="mainContainer">
             <div class="panel panel-info">
                 <div class="panel-heading">News</div>
                 <div class="panel-body">
@@ -27,3 +27,11 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $('#displayAddContactForm').click(function() {
+            $('#mainContainer').load("http://localhost/Syncbook/src/huge/contact/addcontact");
+        });
+
+    });
+</script>
