bdc861f44f0e8955894050646d7c5480cfeb83cd
Enrico <enrico.basso01@gmail.com>
2015-04-10 19:12:30 +0200

    Trying new layout.

--- src/huge/application/view/dashboard/index.php
+++ src/huge/application/view/dashboard/index.php
@@ -1,24 +1,80 @@
 <div class="container">
     <div class="row">
-        <div class="col-sm-2">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Actions</h3>
-                </div>
-                <div class="panel-body">
-                    <ul class="list-group">
-                        <div class="list-group-item " id="displayAddContactForm">
-                            <div class="list-group-item-heading"><strong>Add contact</strong></div>
-                        </div>
-                        <div class="list-group-item ">
-                            <div class="list-group-item-heading"><strong>Contacts</strong></div>
-                        </div>
-                    </ul>
-                </div>
+        <div class="container">
+            <div class="panel">
+                <input type="submit" class="btn btn-primary" id="displayAddContactForm" value="Add contact">
             </div>
         </div>
-        <div class="col-sm-10" id="mainContainer">
-            <div class="panel panel-info">
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Contacts</h3>
+                </div>
+                <nav id="contactList">
+                    <ul class="list-group">
+                        <li class="list-group-item" data-uid=":D">
+                            <div class="col-xs-12 col-sm-3">
+                                <img src="http://api.randomuser.me/portraits/men/97.jpg" alt="Seth Frazier" class="img-responsive img-circle" />
+                            </div>
+                            <div class="col-xs-12 col-sm-9">
+                                <span class="name">Seth Frazier</span><br/>
+                            </div>
+                            <div class="clearfix"></div>
+                        </li>
+                        <li class="list-group-item" data-uid=":D">
+                            <div class="col-xs-12 col-sm-3">
+                                <img src="http://api.randomuser.me/portraits/women/90.jpg" alt="Jean Myers" class="img-responsive img-circle" />
+                            </div>
+                            <div class="col-xs-12 col-sm-9">
+                                <span class="name">Jean Myers</span><br/>
+                            </div>
+                            <div class="clearfix"></div>
+                        </li>
+                        <li class="list-group-item" data-uid=":D">
+                            <div class="col-xs-12 col-sm-3">
+                                <img src="http://api.randomuser.me/portraits/men/24.jpg" alt="Todd Shelton" class="img-responsive img-circle" />
+                            </div>
+                            <div class="col-xs-12 col-sm-9">
+                                <span class="name">Todd Shelton</span><br/>
+                            </div>
+                            <div class="clearfix"></div>
+                        </li>
+                        <li class="list-group-item" data-uid=":D">
+                            <div class="col-xs-12 col-sm-3">
+                                <img src="http://api.randomuser.me/portraits/women/34.jpg" alt="Rosemary Porter" class="img-responsive img-circle" />
+                            </div>
+                            <div class="col-xs-12 col-sm-9">
+                                <span class="name">Rosemary Porter</span><br/>
+                            </div>
+                            <div class="clearfix"></div>
+                        </li>
+                        <li class="list-group-item" data-uid=":D">
+                            <div class="col-xs-12 col-sm-3">
+                                <img src="http://api.randomuser.me/portraits/women/56.jpg" alt="Debbie Schmidt" class="img-responsive img-circle" />
+                            </div>
+                            <div class="col-xs-12 col-sm-9">
+                                <span class="name">Debbie Schmidt</span><br/>
+                            </div>
+                            <div class="clearfix"></div>
+                        </li>
+                        <li class="list-group-item" data-uid=":D">
+                            <div class="col-xs-12 col-sm-3">
+                                <img src="http://api.randomuser.me/portraits/women/76.jpg" alt="Glenda Patterson" class="img-responsive img-circle" />
+                            </div>
+                            <div class="col-xs-12 col-sm-9">
+                                <span class="name">Glenda Patterson</span><br/>
+                            </div>
+                            <div class="clearfix"></div>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+        <div class="col-sm-8" id="mainContainer">
+            <div class="panel panel-success">
                 <div class="panel-heading">News</div>
                 <div class="panel-body">
                     Panel content
@@ -32,6 +88,8 @@
         $('#displayAddContactForm').click(function() {
             $('#mainContainer').load("http://localhost/Syncbook/src/huge/contact/addcontact");
         });
-
+        $('li[data-uid]').click(function() {
+            $('#mainContainer').html($(this).attr('data-uid'));
+        });
     });
 </script>
