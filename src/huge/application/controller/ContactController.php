0de65a8f7adfae6dff5e4aa4f31743c6ab300e8c
Enrico <enrico.basso01@gmail.com>
2015-04-11 17:48:32 +0200

    Created getCardsUID method.

--- src/huge/application/controller/ContactController.php
+++ src/huge/application/controller/ContactController.php
@@ -2,9 +2,9 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationInclude.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationClass.php");
 
-require(SOURCE_PATH . "SabreDAV/CardDAV/cardDAVManagement.php");
-require(SOURCE_PATH . "SabreDAV/CardDAV/contactMapper.php");
-require(SOURCE_PATH . "SabreDAV/CardDAV/vCardManagement.php");
+require_once(SOURCE_PATH . "SabreDAV/CardDAV/cardDAVManagement.php");
+require_once(SOURCE_PATH . "SabreDAV/CardDAV/contactMapper.php");
+require_once(SOURCE_PATH . "SabreDAV/CardDAV/vCardManagement.php");
 
 class ContactController extends Controller {
 
@@ -36,5 +36,23 @@ class ContactController extends Controller {
             // Inserting vCard into Database
             vCardCreate($addressBook, $vCardData);
         }
+    }
+
+    public function displayContactList() {
+        $uriList = ContactModel::getCardsUID();
+
+        if ($uriList !== FALSE) {
+            // Getting PDO Connection for User
+            $connectionPDO = databaseSabreDAVConnectPDO(Session::get('user_name'), new configurationClass());
+
+            // Retrieving AddressBook "Contacts" for User
+            $addressBook = cardDAVAddressBookRetrieve($connectionPDO, Session::get('user_name'), "Contacts");
+
+            if ($addressBook !== FALSE) {
+                $vCardList = vCardListRetrieve($addressBook, $uriList);
+                error_log(print_r($vCardList, TRUE));
+            }
+        }
+
     }
 } 
