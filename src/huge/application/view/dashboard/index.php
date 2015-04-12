7b9bafd02bdf59e4631ff3fc1e06c7ac1b0235aa
Enrico <enrico.basso01@gmail.com>
2015-04-12 21:09:51 +0200

    Added the contact list loading.

--- src/huge/application/view/dashboard/index.php
+++ src/huge/application/view/dashboard/index.php
@@ -13,61 +13,8 @@
                     <h3 class="panel-title">Contacts</h3>
                 </div>
                 <nav id="contactList">
-                    <ul class="list-group">
-                        <li class="list-group-item" data-uid=":D">
-                            <div class="col-xs-12 col-sm-3">
-                                <img src="http://api.randomuser.me/portraits/men/97.jpg" alt="Seth Frazier" class="img-responsive img-circle" />
-                            </div>
-                            <div class="col-xs-12 col-sm-9">
-                                <span class="name">Seth Frazier</span><br/>
-                            </div>
-                            <div class="clearfix"></div>
-                        </li>
-                        <li class="list-group-item" data-uid=":D">
-                            <div class="col-xs-12 col-sm-3">
-                                <img src="http://api.randomuser.me/portraits/women/90.jpg" alt="Jean Myers" class="img-responsive img-circle" />
-                            </div>
-                            <div class="col-xs-12 col-sm-9">
-                                <span class="name">Jean Myers</span><br/>
-                            </div>
-                            <div class="clearfix"></div>
-                        </li>
-                        <li class="list-group-item" data-uid=":D">
-                            <div class="col-xs-12 col-sm-3">
-                                <img src="http://api.randomuser.me/portraits/men/24.jpg" alt="Todd Shelton" class="img-responsive img-circle" />
-                            </div>
-                            <div class="col-xs-12 col-sm-9">
-                                <span class="name">Todd Shelton</span><br/>
-                            </div>
-                            <div class="clearfix"></div>
-                        </li>
-                        <li class="list-group-item" data-uid=":D">
-                            <div class="col-xs-12 col-sm-3">
-                                <img src="http://api.randomuser.me/portraits/women/34.jpg" alt="Rosemary Porter" class="img-responsive img-circle" />
-                            </div>
-                            <div class="col-xs-12 col-sm-9">
-                                <span class="name">Rosemary Porter</span><br/>
-                            </div>
-                            <div class="clearfix"></div>
-                        </li>
-                        <li class="list-group-item" data-uid=":D">
-                            <div class="col-xs-12 col-sm-3">
-                                <img src="http://api.randomuser.me/portraits/women/56.jpg" alt="Debbie Schmidt" class="img-responsive img-circle" />
-                            </div>
-                            <div class="col-xs-12 col-sm-9">
-                                <span class="name">Debbie Schmidt</span><br/>
-                            </div>
-                            <div class="clearfix"></div>
-                        </li>
-                        <li class="list-group-item" data-uid=":D">
-                            <div class="col-xs-12 col-sm-3">
-                                <img src="http://api.randomuser.me/portraits/women/76.jpg" alt="Glenda Patterson" class="img-responsive img-circle" />
-                            </div>
-                            <div class="col-xs-12 col-sm-9">
-                                <span class="name">Glenda Patterson</span><br/>
-                            </div>
-                            <div class="clearfix"></div>
-                        </li>
+                    <ul class="list-group" id="contactListContainer">
+                        <?php ContactModel::getContactListForAddressBook() ?>
                     </ul>
                 </nav>
 
@@ -84,12 +31,19 @@
     </div>
 </div>
 <script>
+    var URL = "http://localhost/Syncbook/src/huge/";
+
+    //$('#contactListContainer').load(URL + "contact/displaycontactlist");
+
     $(document).ready(function() {
+
         $('#displayAddContactForm').click(function() {
-            $('#mainContainer').load("http://localhost/Syncbook/src/huge/contact/addcontact");
+            $('#mainContainer').load(URL + "contact/addcontact");
         });
+
         $('li[data-uid]').click(function() {
             $('#mainContainer').html($(this).attr('data-uid'));
         });
     });
+
 </script>
