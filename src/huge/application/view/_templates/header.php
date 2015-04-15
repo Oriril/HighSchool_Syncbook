74e65dd42cae9e08ccfca662438b8a7d0a0f6ec5
Federico Longhin <longhin.federico.nildric@gmail.com>
2015-04-15 15:49:03 +0200

    Updated page header to be more responsive

--- src/huge/application/view/_templates/header.php
+++ src/huge/application/view/_templates/header.php
@@ -12,6 +12,10 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationInclude.php
 
     <!-- Bootstrap -->
     <link href="../../../lib/bootstrap-3.3.2-dist/css/bootstrap.css" rel="stylesheet">
+    <!-- Base CSS -->
+    <link href="../../../lib/css/base.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -81,5 +85,5 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationInclude.php
         </div>
     </div>
 </nav>
-<div class="container">
+<div class="container containerPage">
     <?php $this->renderFeedbackMessages(); ?>
