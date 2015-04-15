d5379837f7feabfe96a2aeea799535811a0f7801
Federico Longhin <longhin.federico.nildric@gmail.com>
2015-04-15 15:48:39 +0200

    Updated page footer to be more responsive

--- src/huge/application/view/_templates/footer.php
+++ src/huge/application/view/_templates/footer.php
@@ -1,10 +1,9 @@
+</div> <!-- /container -->
+
 <hr>
 <footer>
     <p>&copy; Syncbook 2015</p>
 </footer>
-</div> <!-- /container -->
-
-
 <!-- moved script includes to the header -->
 </body>
 </html>
