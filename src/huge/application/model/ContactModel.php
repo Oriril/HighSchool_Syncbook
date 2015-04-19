4d6f67d9ac2e5ea2e1ae953e0249548acd20cef2
Enrico <enrico.basso01@gmail.com>
2015-04-19 16:52:10 +0200

    Added some "display" function.

--- src/huge/application/model/ContactModel.php
+++ src/huge/application/model/ContactModel.php
@@ -187,6 +187,194 @@ class ContactModel {
         return FALSE;
     }
 
+    public static function printVCard($vCard) {
+        $buttons = "<button type=\"button\" class=\"btn btn-success btn-xs\" id=\"editVCard\" data-uid=\"$vCard->UID\">
+                        <span class=\"glyphicon glyphicon-edit\" aria-hidden=\"true\"></span> Edit
+                    </button>
+                    <button type=\"button\" class=\"btn btn-danger btn-xs\" id=\"deleteVCard\" data-uid=\"$vCard->UID\">
+                        <span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span> Delete
+                     </button>";
+        echo "<nav id=\"mainContainerPanel\"><div class=\"well\"><fieldset><legend>" . $vCard->contactDefault->contactFirstName . " " . $vCard->contactDefault->contactLastName .
+                " " . $buttons . "</legend>";
+        echo "</fieldset></div></nav>";
+    }
+
+    public static function printEditForm($vCard) {
+        $action = Config::get('URL') . "contact/savechangestocontact";
+        echo "<nav id=\"mainContainerPanel\"><div class=\"well\"><form class=\"form-horizontal\" action=\"$action\" method=\"post\"><fieldset><legend>Modify</legend>";
+
+        // first panel
+        echo "<div class=\"col-sm-12\">
+                <div class=\"panel-default\">
+                    <div class=\"panel-body\">
+                        <div class=\"form-group\">
+                            <label for=\"contactPrefix\" class=\"col-lg-2 control-label\">Prefix</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactPrefix\" name=\"contactPrefix\" placeholder=\"Prefix\" value=". $vCard->contactDefault->contactPrefix . ">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactFirstName\" class=\"col-lg-2 control-label\">First Name</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactFirstName\" name=\"contactFirstName\" placeholder=\"First Name\" value=" . $vCard->contactDefault->contactFirstName ." required />
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactMiddleName\" class=\"col-lg-2 control-label\">Middle Name</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactMiddleName\" name=\"contactMiddleName\" placeholder=\"Middle Name\" value=" . $vCard->contactDefault->contactMiddleName . ">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactLastName\" class=\"col-lg-2 control-label\">Last Name</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactLastName\" name=\"contactLastName\" placeholder=\"Last Name\" value=" . $vCard->contactDefault->contactLastName . " required />
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactSuffix\" class=\"col-lg-2 control-label\">Suffix</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactSuffix\" name=\"contactSuffix\" placeholder=\"Suffix\" value=". $vCard->contactDefault->contactSuffix .">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+
+        // company panel
+        echo "<div class=\"col-sm-12\">
+                <div class=\"panel-default\">
+                    <div class=\"panel-body\">
+                        <div class=\"form-group\">
+                            <div class=\"col-lg-2\"></div>
+                            <div class=\"col-lg-10\">
+                                <div class=\"checkbox\">
+                                    <label>
+                                        <input type=\"checkbox\" name=\"contactIsCompany\" value=\"true\">Show as Company
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactCompany\" class=\"col-lg-2 control-label\">Company</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactCompany\" name=\"contactCompany\" placeholder=\"Company name\" value=" . $vCard->contactCompany->contactCompany .">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactDepartment\" class=\"col-lg-2 control-label\">Department</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactDepartment\" name=\"contactDepartment\" placeholder=\"Department\" value=" . $vCard->contactCompany->contactDepartment . ">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactJobTitle\" class=\"col-lg-2 control-label\">Job title</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactJobTitle\" name=\"contactJobTitle\" placeholder=\"Job title\" value=" . $vCard->contactCompany->contactJobTitle .">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactJobRole\" class=\"col-lg-2 control-label\">Job role</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactJobRole\" name=\"contactJobRole\" placeholder=\"Job role\" value=" . $vCard->contactCompany->contactJobRole . ">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <label for=\"contactBirthDate\" class=\"col-lg-2 control-label\">Birthday</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"contactBirthDate\" name=\"contactBirthDate\" placeholder=\"(There will be a select and other stuff)\" value=" . $vCard->contactCompany->contactBirthDate . ">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+
+        // phone
+        echo "<div class=\"col-sm-12\">
+                <div class=\"panel panel-primary\">
+                    <div class=\"panel-heading\">Phone</div>
+                    <div class=\"panel-body\">
+                        <div class=\"form-group\">
+                            <div class=\"col-lg-2\">
+                                <select name=\"phone1Type\">
+                                    <option value=\"home\">Home</option>
+                                    <option value=\"work\">Work</option>
+                                </select>
+                            </div>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"phoneValue\" name=\"phoneValue\" placeholder=\"Phone number\" value=" . $vCard->contactPhone->phoneContainer_1->phoneValue . ">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+
+        // mail
+        echo "<div class=\"col-sm-12\">
+                <div class=\"panel panel-primary\">
+                    <div class=\"panel-heading\">Mail</div>
+                    <div class=\"panel-body\">
+                        <div class=\"form-group\">
+                            <div class=\"col-lg-12\">
+                                <input type=\"text\" class=\"form-control\" id=\"mailValue\" name=\"mailValue\" placeholder=\"Mail\" value=" . $vCard->contactMail->mailContainer_1->mailValue . ">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+
+        // address
+        echo "<div class=\"col-sm-12\">
+                <div class=\"panel panel-primary\">
+                    <div class=\"panel-heading\">Street Address</div>
+                    <div class=\"panel-body\">
+                        <div class=\"form-group\">
+                            <label for=\"addressStreet\" class=\"col-lg-2 control-label\">Home</label>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"addressStreet\" name=\"addressStreet\" placeholder=\"Street\" value=" . $vCard->contactAddress->addressContainer_1->addressStreet . ">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <div class=\"col-lg-2\"></div>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"addressCity\" name=\"addressCity\" placeholder=\"City\" value=" . $vCard->contactAddress->addressContainer_1->addressCity . ">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <div class=\"col-lg-2\"></div>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"addressRegion\" name=\"addressRegion\" placeholder=\"Region\" value=" . $vCard->contactAddress->addressContainer_1->addressRegion .">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <div class=\"col-lg-2\"></div>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"addressPostalCode\" name=\"addressPostalCode\" placeholder=\"Postal code\" value=" . $vCard->contactAddress->addressContainer_1->addressPostalCode . ">
+                            </div>
+                        </div>
+                        <div class=\"form-group\">
+                            <div class=\"col-lg-2\"></div>
+                            <div class=\"col-lg-10\">
+                                <input type=\"text\" class=\"form-control\" id=\"addressCountry\" name=\"addressCountry\" placeholder=\"Country\" value=" . $vCard->contactAddress->addressContainer_1->addressCountry . ">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+
+        // confirm buttons
+        $index = Config::get('URL') . "dashboard/index";
+
+        echo "<div class=\"form-group\">
+                <div class=\"col-sm-4\">
+                    <input type=\"submit\" class=\"btn btn-success btn-lg\" id=\"btn_save\" value=\"Save\">
+                    <a href=\"$index\" class=\"btn btn-danger btn-lg\">Cancel</a>
+            </div>
+            </div>";
+
+        echo "</fieldset></form></div></nav>";
+    }
+
     // Example of Usage with error_log, for testing purpose.
     //error_log(print_r(ContactModel::vCardRetrieve("ff2ba66e-f2a5-4ac0-897c-f322a9f2ede4.vcf"), TRUE));
 }
