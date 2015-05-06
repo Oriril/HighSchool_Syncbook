dc9f307cb06130b8a50171c58d9cdf62de45565c
Enrico <enrico.basso01@gmail.com>
2015-05-06 21:07:36 +0200

    New footer.

--- src/huge/application/view/_templates/footer.php
+++ src/huge/application/view/_templates/footer.php
@@ -1,9 +1,22 @@
-</div> <!-- /container -->
+        </div> <!-- /container -->
 
-<hr>
-<footer>
-    <p>&copy; Syncbook 2015</p>
-</footer>
-<!-- moved script includes to the header -->
-</body>
+        <hr>
+        <footer>
+            <p>&copy; Syncbook 2015</p>
+        </footer>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="../../../lib/js/jquery-1.11.2.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="../../../lib/bootstrap-3.3.4/js/bootstrap.min.js"></script>
+
+        <script src="../../../lib/material-design/js/ripples.min.js"></script>
+        <script src="../../../lib/material-design/js/material.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                // This command is used to initialize some elements and make them work properly
+                $.material.init();
+            });
+        </script>
+        <script src="../../../lib/js/app.js"></script>
+    </body>
 </html>
