c17a4dc4d59555aaebbfea6b870b74985cdf89c7
Enrico <enrico.basso01@gmail.com>
2015-05-08 18:10:00 +0200

    ****

--- src/huge/application/view/login/register.php
+++ src/huge/application/view/login/register.php
@@ -9,86 +9,84 @@
                     <fieldset>
                         <legend>Register a new account</legend>
 
-                        <!-- the user name input field uses a HTML5 pattern check -->
-                        <div class="form-group">
-                            <label for="user_firstname" class="col-lg-2 control-label">First name</label>
+
+                            <div class="form-control-wrapper">
+
+                                    <input type="text" class="form-control floating-label" data-hint="Letters" pattern="[a-zA-Z]" id="user_firstname" name="user_firstname" placeholder="First name" required/>
+
+                            </div><br/>
+
+
+
+                            <div class="form-control-wrapper">
+
+                                    <input type="text" class="form-control floating-label" data-hint="Letters" pattern="[a-zA-Z]" id="user_lastname" name="user_lastname" placeholder="Last name" required/>
+
+                            </div><br/>
+
+
+
+
+                            <div class="form-control-wrapper">
+
+                                    <input type="text" class="form-control floating-label" data-hint="Letters and numbers, 2-64 chars." pattern="[a-zA-Z0-9]{2,64}" id="user_name" name="user_name" placeholder="Username" required />
+
+                            </div><br/>
+
+
+
+                            <div class="form-control-wrapper">
+
+                                    <input type="email" class="form-control floating-label" data-hint="A real address." id="user_email" name="user_email" placeholder="Email" required />
+
+                            </div><br/>
+
+
+                            <div class="form-control-wrapper">
+
+                                    <input type="password" class="form-control floating-label" data-hint="6+ characters." id="user_password_new" name="user_password_new" pattern=".{6,}" placeholder="Password" required autocomplete="off" />
+
+                            </div><br/>
+
+
+                            <div class="form-control-wrapper">
+
+                                    <input type="password" class="form-control floating-label" data-hint="Repeat your password." id="user_password_repeat" name="user_password_repeat" pattern=".{6,}" required placeholder="Repeat your password." autocomplete="off" />
+
+                            </div><br/>
+
+
+
+                            <div class="form-control-wrapper">
+                                <div class="col-lg-2"></div>
                                 <div class="col-lg-10">
-                                    <input type="text" class="form-control" pattern="[a-zA-Z]{2,64}" id="user_firstname" name="user_firstname" placeholder="Your first name (letters)" required/>
+                                <!-- show the captcha by calling the login/showCaptcha-method in the src attribute of the img tag -->
+                                    <img id="captcha" src="<?php echo Config::get('URL'); ?>login/showCaptcha" />
                                 </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="user_lastname" class="col-lg-2 control-label">Last name</label>
-                            <div class="col-lg-10">
-                                <input type="text" class="form-control" pattern="[a-zA-Z]{2,64}" id="user_lastname" name="user_lastname" placeholder="Your last name (letters)" required/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="user_name" class="col-lg-2 control-label">Username</label>
-                            <div class="col-lg-10">
-                                <input type="text" class="form-control" pattern="[a-zA-Z0-9]{2,64}" id="user_name" name="user_name" placeholder="Username (letters/numbers, 2-64 chars)" required />
-                                <span id="helpBlock" class="help-block">letters/numbers, 2-64 chars</span>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="user_email" class="col-lg-2 control-label">Email</label>
-                            <div class="col-lg-10">
-                                <input type="text" class="form-control" id="user_email" name="user_email" placeholder="email address (a real address)" required />
-                                <span id="helpBlock" class="help-block">A real address</span>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="user_password_new" class="col-lg-2 control-label">Password</label>
-                            <div class="col-lg-10">
-                                <input type="password" class="form-control" id="user_password_new" name="user_password_new" pattern=".{6,}" placeholder="Password (6+ characters)" required autocomplete="off" />
-                                <span id="helpBlock" class="help-block">6+ characters</span>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="user_password_repeat" class="col-lg-2 control-label">Repeat Password</label>
-                            <div class="col-lg-10">
-                                <input type="password" class="form-control" id="user_password_repeat" name="user_password_repeat" pattern=".{6,}" required placeholder="Repeat your password" autocomplete="off" />
-                                <span id="helpBlock" class="help-block">Repeat your password</span>
-                            </div>
-                        </div>
+                            </div><br/>
 
-
-                        <div class="form-group">
-                            <div class="col-lg-2"></div>
-                            <div class="col-lg-10">
-                            <!-- show the captcha by calling the login/showCaptcha-method in the src attribute of the img tag -->
-                                <img id="captcha" src="<?php echo Config::get('URL'); ?>login/showCaptcha" />
+                            <div class="form-control-wrapper">
+                                <div class="col-lg-12">
+                                    <input type="text" class="form-control floating-label" data-hint="Please enter above characters" name="captcha" placeholder="Captcha" required />
                                 </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-lg-2"></div>
-                            <div class="col-lg-10">
-                                <input type="text" class="form-control" name="captcha" placeholder="Please enter above characters" required />
-                            </div>
-                        </div>
+                            </div><br/>
 
-                        <div class="form-group">
-                            <div class="col-lg-2"></div>
-                            <div class="col-lg-10">
-                        <!-- quick & dirty captcha reloader -->
-                            <a href="#" style="display: block; font-size: 11px; margin: 5px 0 15px 0; text-align: center"
-                               onclick="document.getElementById('captcha').src = '<?php echo Config::get('URL'); ?>login/showCaptcha?' + Math.random(); return false">Reload Captcha</a>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-lg-2">
-                                <input type="submit" class="btn btn-primary" value="Register" />
-                            </div>
-                        </div>
+                            <div class="form-control-wrapper">
+                                <div class="col-lg-12">
+                            <!-- quick & dirty captcha reloader -->
+                                <a href="#" style="display: block; font-size: 11px; margin: 5px 0 15px 0; text-align: center"
+                                   onclick="document.getElementById('captcha').src = '<?php echo Config::get('URL'); ?>login/showCaptcha?' + Math.random(); return false">Reload Captcha</a>
+                                </div>
+                            </div><br/>
+
+                            <div class="form-control-wrapper">
+                                <div class="col-lg-2">
+                                    <input type="submit" class="btn btn-primary" value="Register" />
+                                </div>
+                            </div><br/>
+
                 </form>
             </div>
         </div>
     </div>
-</div>
-<div class="container">
-    <p style="display: block; font-size: 11px; color: #999;">
-        Please note: This captcha will be generated when the img tag requests the captcha-generation
-        (= a real image) from YOURURL/login/showcaptcha. As this is a client-side triggered request, a
-        $_SESSION["captcha"] dump will not show the captcha characters. The captcha generation
-        happens AFTER the request that generates THIS page has been finished.
-    </p>
 </div>
