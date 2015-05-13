b236346ce013881f91d784554524029fbcef868d
Enrico <enrico.basso01@gmail.com>
2015-05-13 16:10:21 +0200

    I don't know where to put that button.

--- src/huge/application/view/dashboard/index.php
+++ src/huge/application/view/dashboard/index.php
@@ -1,40 +1,38 @@
-<div class="container" id="containerDashboard">
-    <div class="row">
-        <div class="container">
-            <div class="panel">
+<div class="row">
+    <div class="col-sm-1">
+        <button class="btn btn-fab btn-raised btn-primary btn-xs" id="displayAddContactForm"><i class="mdi-content-add-circle"></i></button>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-4">
+        <div class="panel panel-primary panel-no-margin-bottom">
+            <div class="panel-heading">
+                <div class="float-left">
+                    <h3 class="panel-title">Contacts</h3>
+                </div>
+                <!--<div class="float-right">
 
+                    <button class="btn btn-primary fa fa-plus-circle" id="displayAddContactForm"></button>
+                </div>-->
+                <div class="clear"></div>
             </div>
+            <nav id="contactList">
+                <div class="list-group" id="contactListContainer">
+                    <?php ContactModel::getContactListForAddressBook(); ?>
+                </div>
+            </nav>
+
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-4">
-            <div class="panel panel-primary panel-no-margin-bottom">
-                <div class="panel-heading">
-                    <div class="float-left">
-                        <h3 class="panel-title">Contacts</h3>
-                    </div>
-                    <div class="float-right">
-                        <button class="btn btn-primary fa fa-plus-circle" id="displayAddContactForm"></button>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-                <nav id="contactList">
-                    <div class="list-group" id="contactListContainer">
-                        <?php ContactModel::getContactListForAddressBook(); ?>
-                    </div>
-                </nav>
-
-            </div>
-        </div>
-        <div class="col-sm-8" id="mainContainer">
-            <div class="panel panel-success">
-                <div class="panel-heading">News</div>
-                <div class="panel-body">
-                    Panel content
-                </div>
+    <div class="col-sm-8" id="mainContainer">
+        <div class="panel panel-success">
+            <div class="panel-heading">News</div>
+            <div class="panel-body">
+                Panel content
             </div>
         </div>
     </div>
 </div>
+
 
 
