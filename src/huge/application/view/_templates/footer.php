66f7bb53f3569d5ac9faa594aeef8c4164908772
Enrico <enrico.basso01@gmail.com>
2015-05-13 16:08:27 +0200

    Using bootstrap 3.3.2.

--- src/huge/application/view/_templates/footer.php
+++ src/huge/application/view/_templates/footer.php
@@ -7,10 +7,10 @@
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="../../../lib/js/jquery-1.11.2.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="../../../lib/bootstrap-3.3.4/js/bootstrap.min.js"></script>
+        <script src="../../../lib/bootstrap-3.3.2/js/bootstrap.min.js"></script>
 
-        <script src="../../../lib/material-design/js/ripples.min.js"></script>
-        <script src="../../../lib/material-design/js/material.min.js"></script>
+        <script src="../../../lib/material-design/js/ripples.js"></script>
+        <script src="../../../lib/material-design/js/material.js"></script>
         <script>
             $(document).ready(function() {
                 // This command is used to initialize some elements and make them work properly
