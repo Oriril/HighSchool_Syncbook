53875bd0bb50a2dae05d090314987db301a1c23e
Enrico <enrico.basso01@gmail.com>
2015-05-14 23:01:02 +0200

    Added new js

--- src/huge/application/view/_templates/footer.php
+++ src/huge/application/view/_templates/footer.php
@@ -11,6 +11,11 @@
 
         <script src="../../../lib/material-design/js/ripples.js"></script>
         <script src="../../../lib/material-design/js/material.js"></script>
+
+        <!-- Scrolling Nav JavaScript -->
+        <script src="../../../lib/js/jquery.easing.min.js"></script>
+        <script src="../../../lib/js/scrolling-nav.js"></script>
+
         <script>
             $(document).ready(function() {
                 // This command is used to initialize some elements and make them work properly
