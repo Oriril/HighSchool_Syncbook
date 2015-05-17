0c6ec904ae438de1467c922660ef00c22307ffe9
Federico Longhin <longhin.federico.nildric@gmail.com>
2015-05-17 19:23:22 +0200

    Removed "News" title because it was meaningless. Added a "Plus" Button to Add contacts, that will be kept if my co-worker is cool with that

--- src/huge/application/view/dashboard/index.php
+++ src/huge/application/view/dashboard/index.php
@@ -4,7 +4,7 @@
             <div class="panel panel-primary panel-no-margin-bottom">
                 <div class="panel-heading">
                     <div class="float-left">
-                        <h3 class="panel-title">Contacts</h3>
+                        <h3 class="panel-title">Contacts List</h3>
                     </div>
                     <!--<div class="float-right">
 
@@ -12,22 +12,37 @@
                     </div>-->
                     <div class="clear"></div>
                 </div>
+
+                <br>
+                <div style="text-align: center;">
+                    <button id="displayAddContactForm" class="btn btn-fab btn-fab-mini btn-sm btn-raised btn-primary">
+                        <i class='mdi-content-add'></i>
+                    </button>
+                </div>
+                <br>
+                <!-- <hr style="border-top: dashed 2px;"> -->
+
                 <nav id="contactList">
                     <div class="list-group" id="contactListContainer">
                         <?php ContactModel::getContactListForAddressBook(); ?>
                     </div>
                 </nav>
+            </div>
+        </div>
+        <div class="col-sm-8" id="mainContainer"/>
 
-            </div>
-        </div>
-        <div class="col-sm-8" id="mainContainer">
-            <div class="panel panel-success">
-                <div class="panel-heading">News</div>
-                <div class="panel-body">
-                    Panel content
+        <?php
+            /*
+                <div class="col-sm-8" id="mainContainer">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">News</div>
+                        <div class="panel-body">
+                            Panel content
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            */
+        ?>
     </div>
 </div>
 
