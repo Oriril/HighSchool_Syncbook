09dd78d990904f767024070d1aa449a8be7b951f
Enrico <enrico.basso01@gmail.com>
2015-05-19 16:34:22 +0200

    Added new css.

--- src/huge/application/view/error/index.php
+++ src/huge/application/view/error/index.php
@@ -1,6 +1,10 @@
-<div class="container  fix-navbar">
-    <h1>Page not found</h1>
-    <div class="box">
-        <p class="red-text">This page does not exist.</p>
+<div class="container fix-navbar">
+    <div class="panel-danger">
+        <div class="panel-heading">
+            <h1>Page not found</h1>
+        </div>
+        <div class="panel-body">
+            <p>This page does not exist.</p>
+        </div>
     </div>
 </div>
