024fd53314b0d7540465f5d252b6ea59ec326012
Enrico <enrico.basso01@gmail.com>
2015-05-19 18:08:39 +0200

    New index

--- src/huge/application/view/index/index.php
+++ src/huge/application/view/index/index.php
@@ -2,17 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Enjoy</h1>
-                <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
-            </div>
-        </div>
-    </div>
-</section>
-<section id="about" class="about-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>About Section</h1>
+                <h1>Welcome to Syncbook</h1>
+                <p>Some text here</p>
+                <a class="btn btn-raised page-scroll" href="#services">See more</a>
             </div>
         </div>
     </div>
@@ -21,17 +13,74 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Services Section</h1>
+                <h1>What can I do with Syncbook?</h1>
+            </div>
+        </div>
+        <div class="row" style="text-align: left;">
+            <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-sm-6">Random image of a syncing process.</div>
+        </div>
+    </div>
+</section>
+<section id="powered-by" class="powered-by-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="well">
+                    <h1>PHP</h1>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="well">
+                    <h1>CSS3 & Bootstrap</h1>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="well">
+                    <h1>jQuery</h1>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="well">
+                    <h1>Sabre</h1>
+                </div>
             </div>
         </div>
     </div>
 </section>
-<section id="contact" class="contact-section">
+<section id="about" class="about-section">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <h1>Contact Section</h1>
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <canvas class="header-bg" width="250" height="70" id="header-blur"></canvas>
+                    <div class="avatar">
+                        <img src="" alt="" />
+                    </div>
+                    <div class="content">
+                        <p>Web Developer <br>
+                            More description here</p>
+                        <p><button type="button" class="btn btn-default">Contact</button></p>
+                    </div>
+                </div>
             </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <canvas class="header-bg" width="250" height="70" id="header-blur"></canvas>
+                    <div class="avatar">
+                        <img src="http://www.gravatar.com/avatar/8d669acbd39ba16bb05a92e2937b2c70.png" alt="" />
+                    </div>
+                    <div class="content">
+                        <p>Web Developer <br>
+                            More description here</p>
+                        <p><button type="button" class="btn btn-default">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3"></div>
         </div>
     </div>
 </section>
