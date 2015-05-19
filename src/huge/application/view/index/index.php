b7188da39ddb97c3dbc2f42d2f0645937803658c
Xooline <enrico.basso01@gmail.com>
2015-05-19 14:35:57 +0200

    New layout.

--- src/huge/application/view/index/index.php
+++ src/huge/application/view/index/index.php
@@ -40,7 +40,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <!--<h4 class="modal-title">Log in</h4>-->
+                <h4 class="modal-title">Log in</h4>
             </div>
             <div class="modal-body">
                 <form class="form-horizontal" action="<?php echo Config::get('URL'); ?>login/login" method="post">
