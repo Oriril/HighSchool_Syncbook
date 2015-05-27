c2e9e0abb50781717fe3dcf01ed320c815052f53
Enrico <enrico.basso01@gmail.com>
2015-05-27 16:54:13 +0200

    New profile interface.

--- src/huge/application/view/login/showProfile.php
+++ src/huge/application/view/login/showProfile.php
@@ -1,20 +1,25 @@
 <div class="container fix-navbar">
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
-
-    <div class="col-sm-8">
-        <div class="panel-warning">
-            <div class="panel-heading"><?= $this->user_name; ?></div>
-            <div class="panel-body">
-                <div class="col-sm-6">Your email:</div>
-                <div class="col-sm-6"><?= $this->user_email; ?></div>
-                <div class="col-sm-6">Your avatar image (on gravatar.com):</div>
-                <div class="col-sm-6">
-                         <img src='<?= $this->user_gravatar_image_url; ?>' />
-
+    <div class="col-sm-4"></div>
+    <div class="col-sm-4">
+        <div class="well no-padding" style="padding: 0px;">
+            <div class="card hovercard" style="display: block;">
+                <div class="cardheader">
                 </div>
+                <div class="avatar">
+                    <img alt="" src="<?= $this->user_gravatar_image_url; ?>">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="https://twitter.com/LonghinFederico"><?= $this->user_name; ?></a>
+                    </div>
+                    <div class="desc"><?= $this->user_email; ?></div>
+                    <div class="desc">Profile picture powered by <i>gravatar.com</i></div>
+                </div>
+                <div class="bottom"></div>
             </div>
-
         </div>
     </div>
+    <div class="col-sm-4"></div>
 </div>
