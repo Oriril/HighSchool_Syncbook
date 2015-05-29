76dab149510c15f868c2d83a5f65d621fd21cad0
Federico Longhin <longhin.federico.nildric@gmail.com>
2015-05-29 18:25:50 +0200

    Removed some files from testing folder because now useless.

--- test/hugeTesting.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo ('https://' . $_SERVER['HTTP_HOST'] . str_replace('public', '', dirname($_SERVER['SCRIPT_NAME'])));
-$vendorDir = dirname(dirname(__FILE__));
-//$baseDir = dirname($vendorDir);
-echo($vendorDir);
