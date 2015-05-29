08b016f92c9ea00a0b60b11dfbe5456b1106e30f
Federico Longhin <longhin.federico.nildric@gmail.com>
2015-05-29 18:59:11 +0200

    Git Documentation Branch First Commit

--- cfg/configurationInclude.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined("LIBRARY_PATH") or define("LIBRARY_PATH", dirname(__DIR__) . "/lib/");
-defined("SOURCE_PATH") or define("SOURCE_PATH", dirname(__DIR__) . "/src/");
-defined("SQL_PATH") or define("SQL_PATH", dirname(__DIR__) . "/sql/");
-defined("TEST_PATH") or define("TEST_PATH", dirname(__DIR__) . "/test/");
-
-require_once(LIBRARY_PATH . "SabreDAV/vendor/autoload.php");
-require_once(LIBRARY_PATH . "RedBeanPHP.php");
--- doc/Doxygen/SabreDAV/html/class_sabre_1_1_d_a_v_1_1_property_1_1_response_list_test.js
+++
@@ -1,4 +0,0 @@
-var class_sabre_1_1_d_a_v_1_1_property_1_1_response_list_test =
-[
-    [ "testInvalidArg", "class_sabre_1_1_d_a_v_1_1_property_1_1_response_list_test.html#a9dd6989deb1be14024415c5c098cdfd4", null ]
-];
