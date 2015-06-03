aa4d9446d44ff81972e235a64ce9b5a8264c98c2
Enrico <enrico.basso01@gmail.com>
2015-06-03 19:43:55 +0200

    Removed a useless class on navbar.

--- src/huge/application/view/_templates/header.php
+++ src/huge/application/view/_templates/header.php
@@ -38,80 +38,80 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/Syncbook/cfg/configurationInclude.php
 
 </head>
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-<nav class="navbar navbar-inverse navbar-default navbar-fixed-top top-nav-collapse" role="navigation">
-    <div class="container">
-        <div class="navbar-header page-scroll">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand page-scroll" href="#page-top">Syncbook</a>
-        </div>
+    <nav class="navbar navbar-inverse navbar-fixed-top top-nav-collapse" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Syncbook</a>
+            </div>
 
-        <div class="collapse navbar-collapse" id="navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="hidden">
-                    <a class="page-scroll" href="#page-top"></a>
-                </li>
-                <?php if (View::checkForActiveController($filename, "index")) { ?>
-                    <li>
-                        <a class="page-scroll" href="#services">Services</a>
+            <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
                     </li>
-                    <li>
-                        <a class="page-scroll" href="#powered-by">Powered by</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#team">Team</a>
-                    </li>
-                <?php } else { ?>
-                    <li>
-                        <a href="<?php echo Config::get('URL'); ?>index/index">Home</a>
-                    </li>
-                <?php } ?>
-                <?php if (Session::userIsLoggedIn()) { ?>
-                    <li id="page-dashboard">
-                        <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
-                    </li>
-                <?php } ?>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <?php if (Session::userIsLoggedIn()) { ?>
-                    <?php if (View::checkForActiveController($filename, "dashboard")) { ?>
-                        <li class="add-button">
-                            <button class="btn btn-fab btn-fab-mini btn-raised btn-material-deep-purple-200 btn-sm" id="displayAddContactForm">
-                                <i class='fa fa-plus'></i>
-                            </button>
-                        </li>
-                    <?php } ?>
-                    <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="dropdown" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>login/showprofile" class="dropdown-toggle" style="padding-bottom: 8px;padding-top: 8px;" data-toggle="dropdown" role="button" aria-expanded="false"><img src="<?php echo Session::get('user_gravatar_image_url') ?>" class="img-circle"><span class="caret"></span></a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                                <a href="<?php echo Config::get('URL'); ?>login/showprofile">Show profile</a>
-                            </li>
-                            <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                                <a href="<?php echo Config::get('URL'); ?>login/edituseremail">Edit my email</a>
-                            </li>
-                            <li class="divider"></li>
-                            <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                                <a href="<?php echo Config::get('URL'); ?>login/logout">Logout</a>
-                            </li>
-                        </ul>
-                    </li>
-                <?php } else { ?>
-                    <?php if (!View::checkForActiveController($filename, "login")) { ?>
+                    <?php if (View::checkForActiveController($filename, "index")) { ?>
                         <li>
-                            <button class="btn btn-material-deep-purple-300" data-toggle="modal" data-target="#log-in-dialog">Log in</button>
+                            <a class="page-scroll" href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#powered-by">Powered by</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#team">Team</a>
+                        </li>
+                    <?php } else { ?>
+                        <li>
+                            <a href="<?php echo Config::get('URL'); ?>index/index">Home</a>
                         </li>
                     <?php } ?>
+                    <?php if (Session::userIsLoggedIn()) { ?>
+                        <li id="page-dashboard">
+                            <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if (Session::userIsLoggedIn()) { ?>
+                        <?php if (View::checkForActiveController($filename, "dashboard")) { ?>
+                            <li class="add-button">
+                                <button class="btn btn-fab btn-fab-mini btn-raised btn-material-deep-purple-200 btn-sm" id="displayAddContactForm">
+                                    <i class='fa fa-plus'></i>
+                                </button>
+                            </li>
+                        <?php } ?>
+                        <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="dropdown" '; } ?> >
+                            <a href="<?php echo Config::get('URL'); ?>login/showprofile" class="dropdown-toggle" style="padding-bottom: 8px;padding-top: 8px;" data-toggle="dropdown" role="button" aria-expanded="false"><img src="<?php echo Session::get('user_gravatar_image_url') ?>" class="img-circle"><span class="caret"></span></a>
 
-                <?php } ?>
-            </ul>
+                            <ul class="dropdown-menu" role="menu">
+                                <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
+                                    <a href="<?php echo Config::get('URL'); ?>login/showprofile">Show profile</a>
+                                </li>
+                                <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
+                                    <a href="<?php echo Config::get('URL'); ?>login/edituseremail">Edit my email</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
+                                    <a href="<?php echo Config::get('URL'); ?>login/logout">Logout</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <?php } else { ?>
+                        <?php if (!View::checkForActiveController($filename, "login")) { ?>
+                            <li>
+                                <button class="btn btn-material-deep-purple-300" data-toggle="modal" data-target="#log-in-dialog">Log in</button>
+                            </li>
+                        <?php } ?>
+
+                    <?php } ?>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
 <!-- Header -->
