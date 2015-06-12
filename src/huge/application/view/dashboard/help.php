36f271caf3d6922aa148c89c762e1d4b2ea3d030
Enrico <enrico.basso01@gmail.com>
2015-06-12 15:03:10 +0200

    Help page.

--- src/huge/application/view/dashboard/help.php
+++ src/huge/application/view/dashboard/help.php
@@ -1,3 +1,66 @@
 <div class="container containerPage fix-navbar" id="containerPage">
-
+    <div class="row">
+        <h1 style="text-align: center;">How to connect your Android device to our server</h1>
+    </div>
+    <div class="row" style="padding-bottom: 20px;">
+        <div class="col-sm-6">
+            <img src="http://i.imgur.com/fFsHmob.png" class="img-responsive help center">
+        </div>
+        <div class="col-sm-6">
+            <p>
+                At the moment we only support <a href="https://play.google.com/store/apps/details?id=org.dmfs.carddav.sync" alt="CardDAV-Sync" target="_blank">CardDAV-Sync</a>.
+                After you have installed the application, open it and select "Add account".
+                You will see something like the image at the left, then select the first option "CardAV".
+            </p>
+        </div>
+    </div>
+    <div class="row" style="padding-bottom: 20px;">
+        <div class="col-sm-6">
+            <img src="http://i.imgur.com/RNhVLZb.png" class="img-responsive help center">
+        </div>
+        <div class="col-sm-6">
+            <p>
+                Now you need to complete the form with se following parameters:<br>
+                - Server name: <strong>dav.syncbook.me/groupwareserver.php/principals/&ltusername&gt</strong>, where <strong>&ltusername&gt</strong> is the one you
+                    used to register on Syncbook.<br>
+                - Username: the one you used to register on Syncbook.<br>
+                - Password: the one you used to register on Syncbook.<br>
+                - Client certificate: none.<br>
+                Now press "NEXT".
+            </p>
+        </div>
+    </div>
+    <div class="row" style="padding-bottom: 20px;">
+        <div class="col-sm-6">
+            <img src="http://i.imgur.com/ETmW51r.png" class="img-responsive help center">
+        </div>
+        <div class="col-sm-6">
+            <p>
+                Accept this warning message.
+            </p>
+        </div>
+    </div>
+    <div class="row" style="padding-bottom: 20px;">
+        <div class="col-sm-6">
+            <img src="http://i.imgur.com/DiXQ7HJ.png" class="img-responsive help center">
+        </div>
+        <div class="col-sm-6">
+            <p>
+                Well done! Now you've properly configured your device to keep synchronized with our server.
+                You can check the settings for advanced options.
+            </p>
+        </div>
+    </div>
+    <div class="row" style="padding-bottom: 20px;">
+        <div class="col-sm-6">
+            <img src="http://i.imgur.com/gH5gaqv.png" class="img-responsive help center">
+        </div>
+        <div class="col-sm-6" style="padding-bottom: 20px;">
+            <p>
+                Now if you check your contacts list of your smartphone, you will see the contacts added in syncbook.me.
+                If you want to see only contacts from syncbook.me, check the option "Contacts" with the <a href="https://play.google.com/store/apps/details?id=org.dmfs.carddav.sync" alt="CardDAV-Sync" target="_blank">CardDAV-Sync</a>
+                icon.
+            </p>
+        </div>
+    </div>
 </div>
